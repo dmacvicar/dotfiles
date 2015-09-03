0fdeda4dc4dbb53d12784008d18c45f1b508a103
Duncan Mac-Vicar P <dmacvicar@suse.de>
2015-09-03 09:30:41 +0200

    show internal display

--- i3/.i3/dock.sh
+++ i3/.i3/dock.sh
@@ -12,7 +12,7 @@ elif [ $displayCount -eq 2 ]; then
     echo "Enabling home dock"
     xrandr --output $ext1Display --primary --output $intDisplay --off
 elif [ $displayCount -eq 1 ]; then
-    echo "Enabling one display"
+    echo "Enabling one display $intDisplay"
     xrandr --output $intDisplay --primary
 fi
 
