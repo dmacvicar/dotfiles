310c5dd97abef1eff7a57ac7038570bdad61e875
Duncan Mac-Vicar P <dmacvicar@suse.de>
2015-09-07 14:49:47 +0200

    add GOPATH

---
+++ zsh/.zsh/exports.zsh
@@ -0,0 +1,8 @@
+export EDITOR="emacs-nox"
+export VISUAL="${EDITOR}"
+
+# color for less
+export LESSOPEN="| /usr/bin/src-hilite-lesspipe.sh %s"
+export LESS=' -R '
+
+export GOPATH=/space/git/golang
