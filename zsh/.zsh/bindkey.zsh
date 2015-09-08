ac8d37895fdf939ae820142190b3efda48ef6569
Duncan Mac-Vicar P <dmacvicar@suse.de>
2015-09-08 14:25:04 +0200

    fix history with arrow keys

--- zsh/.zsh/bindkey.zsh
+++ zsh/.zsh/bindkey.zsh
@@ -4,10 +4,10 @@
 
 bindkey "^K"      kill-whole-line                      # ctrl-k
 bindkey "^R"      history-incremental-search-backward  # ctrl-r
-bindkey "^A"      beginning-of-line                    # ctrl-a  
+bindkey "^A"      beginning-of-line                    # ctrl-a
 bindkey "^E"      end-of-line                          # ctrl-e
-bindkey "[B"      history-search-forward               # down arrow
-bindkey "[A"      history-search-backward              # up arrow
+bindkey "^[[B"    history-beginning-search-forward     # down arrow
+bindkey "^[[A"    history-beginning-search-backward    # up arrow
 bindkey "^D"      delete-char                          # ctrl-d
 bindkey "^F"      forward-char                         # ctrl-f
 bindkey "^B"      backward-char                        # ctrl-b
