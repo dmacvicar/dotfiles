0f6fd0d77b0c7142129780b304009089eed3ffc0
Duncan Mac-Vicar P <dmacvicar@suse.de>
2016-09-22 12:40:32 +0200

    refactor work related extra config handling

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -390,9 +390,11 @@ It runs `tabulated-list-revert-hook', then calls `tabulated-list-print'."
               vc-ignore-dir-regexp
               tramp-file-name-regexp))
 
-(if (file-exists-p "~/.mu4e/mu4e.suse.el")
-    (load-file "~/.mu4e/mu4e.suse.el"))
+;; work setup
+(if (file-exists-p "~/.emacs.suse.d/init.el")
+    (load-file "~/.emacs.suse.d/init.el"))
 
+(require 'org)
 (setq org-src-fontify-natively t)
 (setq org-fontify-whole-heading-line t)
 ;; Org mode
