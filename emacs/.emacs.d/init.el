1b41924a33a7605cbe40a6fb1250a1d31dc98a01
Duncan Mac-Vicar P <dmacvicar@suse.de>
2017-11-20 20:46:09 +0100

    add org mode repo

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -11,6 +11,7 @@
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
 (package-initialize)
 
 (unless (package-installed-p 'use-package)
