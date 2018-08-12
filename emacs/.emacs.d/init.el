41efa713942a8d3510b73f61f3edf0405feaf9c4
Duncan Mac-Vicar P <dmacvicar@suse.de>
2018-08-12 13:40:44 +0200

    Marmalade certificate expired

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -12,7 +12,6 @@
 (require 'package)
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
 (package-initialize)
