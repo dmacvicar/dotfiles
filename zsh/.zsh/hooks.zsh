cba05e65847db72119edb55a85dda44923cd261b
Duncan Mac-Vicar P <dmacvicar@suse.de>
2018-11-26 11:58:29 +0100

    Fix set_running_app:printf:1: %{: invalid directive

--- zsh/.zsh/hooks.zsh
+++ zsh/.zsh/hooks.zsh
@@ -8,7 +8,7 @@ function precmd {
 }
 
 function set_running_app {
-  printf "\e]1; $PWD:t:$(history $HISTCMD | cut -b7- ) \a"
+  echo "\e]1; $PWD:t:$(history $HISTCMD | cut -b7- ) \a"
 }
 
 function preexec {
