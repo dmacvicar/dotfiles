9d17089aef346af1c69fc9d241863942dda83c96
Duncan Mac-Vicar P <dmacvicar@suse.de>
2019-06-21 21:20:13 +0200

    emacs: remove scroll bar

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -7,7 +7,7 @@
 ;; do this early
 (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
-;;(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 
 (require 'package)
 (setq package-enable-at-startup nil)
