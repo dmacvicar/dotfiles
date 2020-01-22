bcb9de17c82f7cf54224e2602445aa907b2cf80a
Duncan Mac-Vicar P <dmacvicar@suse.de>
2020-01-22 20:57:33 +0100

    Do not add ELPA by default

--- emacs/.emacs.d/early-init.el
+++ emacs/.emacs.d/early-init.el
@@ -3,6 +3,7 @@
 (setq gc-cons-threshold (* 50 1000 1000))
 (defconst emacs-start-time (current-time))
 (require 'package)
+(setq package-archives '())
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 ; https://debbugs.gnu.org/cgi/bugreport.cgi?bug=34341
 ; should be fixed in Emacs 26.3+
