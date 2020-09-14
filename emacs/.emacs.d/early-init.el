aee60bfb23769e7f68c9e2f5dde215e6b61c907e
Duncan Mac-Vicar P <dmacvicar@suse.de>
2020-09-14 09:32:06 +0200

    Set archive priorities

--- emacs/.emacs.d/early-init.el
+++ emacs/.emacs.d/early-init.el
@@ -3,10 +3,14 @@
 (setq gc-cons-threshold (* 50 1000 1000))
 (defconst emacs-start-time (current-time))
 (require 'package)
-(setq package-archives '())
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 ; https://debbugs.gnu.org/cgi/bugreport.cgi?bug=34341
 ; should be fixed in Emacs 26.3+
-(setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
-(add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/") t)
-(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
+;(setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
+(setq package-archives
+      '(("elpa" . "https://elpa.gnu.org/packages/")
+        ("melpa" . "https://melpa.org/packages/")
+        ("org" . "https://orgmode.org/elpa/"))
+      package-archive-priorities
+      '(("org" . 10)
+        ("melpa" . 5)
+        ("elpa" . 0)))
