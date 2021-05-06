bc8d561fce98d148a42b3688624f933ff6f86bb3
Duncan Mac-Vicar P <duncan@mac-vicar.eu>
2021-05-06 14:19:38 +0200

    Update doom modules

--- emacs/.config/doom/init.el
+++ emacs/.config/doom/init.el
@@ -1,43 +1,43 @@
 (defconst mu4e-system-path "/usr/share/emacs/site-lisp/mu4e")
 (add-to-list 'load-path mu4e-system-path)
 
-(doom! :lang
+(doom! :checkers
+       (spell +flyspell +hunspell +aspell +everywhere)
+       syntax
+       ophints
+       minimap
+       modeline
+       :completion
+       (company +childframe)
+       (ivy +fuzzy +childframe)
+       :emacs
+       dired
+       :lang
        (cc +lsp)
        (go +lsp)
        (json +lsp)
        markdown
        (org +babel +export +present +pandoc +pretty)
-       python
+       (python)
        rest
-       ruby
+       (ruby +lsp)
        sh
        yaml
-       :emacs
-       dired
-       :completion
-       (company +childframe)
-       (ivy +fuzzy +childframe)
+       :mail
+       mu4e
+       :os
+       (popup +all +defaults)
+       tty
+       :term
+       shell
        :tools
        (eval +overlay)
        lookup
        (debugger +lsp)
        lsp
        magit
-       :checkers
-       (spell +flyspell +hunspell +aspell +everywhere)
-       syntax
-       ophints
-       minimap
-       modeline
        :ui
        doom
        doom-dashboard
        treemacs
-       workspaces
-       ;indent-guides
-       ;tabs
-       :os
-       (popup +all +defaults)
-       tty
-       :mail
-       mu4e)
+       workspaces)
