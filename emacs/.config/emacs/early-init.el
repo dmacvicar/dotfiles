61ea9c89d486df74731a07bf9a53131cde8937dd
Duncan Mac-Vicar P <duncan@mac-vicar.eu>
2022-04-20 20:01:15 +0200

    emacs: fix cache redirection

--- emacs/.config/emacs/early-init.el
+++ emacs/.config/emacs/early-init.el
@@ -1,6 +1,7 @@
 (setq package-enable-at-startup nil)
 
 (when (fboundp 'startup-redirect-eln-cache)
+  (require 'xdg)
   (startup-redirect-eln-cache
    (convert-standard-filename
       (expand-file-name  "emacs/eln-cache" (xdg-cache-home)))))
