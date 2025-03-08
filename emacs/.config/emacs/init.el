a44020b32495aaed11563fa881deb87318721c74
Duncan Mac-Vicar P. <duncan@mac-vicar.eu>
2025-03-08 10:31:32 +0100

    emacs: move function near to where is used

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -19,25 +19,6 @@
 (when (file-exists-p custom-file)
     (load custom-file :noerror))
 
-;; nicer default frame size depending of screen
-(defun duncan/set-frame-size-according-to-resolution ()
-  (interactive)
-  (when (display-graphic-p)
-  (progn
-    ;; use 120 char wide window for largeish displays
-    ;; and smaller 80 column windows for smaller displays
-    ;; pick whatever numbers make sense for you
-    (if (> (x-display-pixel-width) 1280)
-           (add-to-list 'default-frame-alist (cons 'width 120))
-           (add-to-list 'default-frame-alist (cons 'width 80)))
-    ;; for the height, subtract a couple hundred pixels
-    ;; from the screen height (for panels, menubars and
-    ;; whatnot), then divide by the height of a char to
-    ;; get the height we want
-    (add-to-list 'default-frame-alist
-         (cons 'height (/ (- (x-display-pixel-height) 200)
-                             (frame-char-height)))))))
-
 (add-hook 'prog-mode-hook 'display-line-numbers-mode)
 (add-hook 'prog-mode-hook (lambda () (setq show-trailing-whitespace t)))
 (add-hook 'text-mode-hook (lambda () (setq show-trailing-whitespace t)))
@@ -405,6 +386,25 @@
   :defer t
   :custom
   (mixed-pitch-variable-pitch-cursor '(bar . 3)))
+
+;; nicer default frame size depending of screen
+(defun duncan/set-frame-size-according-to-resolution ()
+  (interactive)
+  (when (display-graphic-p)
+  (progn
+    ;; use 120 char wide window for largeish displays
+    ;; and smaller 80 column windows for smaller displays
+    ;; pick whatever numbers make sense for you
+    (if (> (x-display-pixel-width) 1280)
+           (add-to-list 'default-frame-alist (cons 'width 120))
+           (add-to-list 'default-frame-alist (cons 'width 80)))
+    ;; for the height, subtract a couple hundred pixels
+    ;; from the screen height (for panels, menubars and
+    ;; whatnot), then divide by the height of a char to
+    ;; get the height we want
+    (add-to-list 'default-frame-alist
+         (cons 'height (/ (- (x-display-pixel-height) 200)
+                             (frame-char-height)))))))
 
 (duncan/set-frame-size-according-to-resolution)
 ;; not decided yet if maximizing is better?
