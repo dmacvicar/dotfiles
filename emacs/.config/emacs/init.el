c00c10678613058e575345fe723f74ca3eb6df5c
Duncan Mac-Vicar P. <duncan@mac-vicar.eu>
2026-03-04 19:51:31 +0100

    Guard treesit-auto load on startup

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -737,15 +737,17 @@ will be selected, otherwise a light theme will be selected (0 is default)"
     (add-to-list 'major-mode-remap-alist mode)))
 
 (use-package treesit-auto
-  :hook (after-init . global-treesit-auto-mode)
   :custom
   (treesit-auto-install 'prompt)
   ; those are broken
   (treesit-auto-opt-out-list
         '(markdown protobuf ruby r yaml))
-  )
-;; ensure treesit-auto is installed before after-init hooks run
-(elpaca-wait)
+  :init
+  ;; Avoid startup error if package isn't installed yet.
+  (add-hook 'after-init-hook
+            (lambda ()
+              (when (require 'treesit-auto nil t)
+                (global-treesit-auto-mode 1)))))
 
 ;; structured navigation including expand region
 (use-package combobulate
