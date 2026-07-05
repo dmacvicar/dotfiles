d7e219c48dc4c641c64dc69bde29438b29bddd16
Duncan Mac-Vicar P. <duncan@mac-vicar.eu>
2026-07-05 15:43:40 +0200

    emacs: fix dtrt-indent-mode

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -926,7 +926,8 @@ will be selected, otherwise a light theme will be selected (0 is default)"
   (dtrt-indent-max-lines 2000)
   (dtrt-indent-verbosity 2)
   :diminish
-  :hook (elpaca-after-init-hook . dtrt-indent-mode))
+  :hook
+  ((prog-mode-hook text-mode-hook) . dtrt-indent-mode))
 (setq-default tab-width 8)
 
 (use-package paredit
